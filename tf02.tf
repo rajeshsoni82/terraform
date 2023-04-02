ac1157d19375acc0667e3e788894bc0e47ef49f4
rajeshsoni82 <rajeshsoni82@gmail.com>
2023-04-02 08:45:32 -0400

    Create tf02.tf

---
+++ tf02.tf
@@ -0,0 +1,16 @@
+provider "azureem" {
+    version ="=2.4.0"
+    features {}
+}
+
+resource "azurerm_resource_group" {
+
+name = "rgm_1"
+location = "East US"
+
+}
+
+output "id" {
+
+    value = data.azurerm_resource_group.example.id
+}
